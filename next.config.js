a9dca2c84cab001be9fd1240c9046b16432f85f3
kanak99999 <kanaksharma.knk@gmail.com>
2025-04-06 18:49:09 +0530

    fixed ts config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  transpilePackages: [
+    'antd',
+    'rc-util',
+    'rc-picker',
+    '@ant-design/icons',
+    '@ant-design/icons-svg'
+  ],
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      extensionAlias: {
+        '.js': ['.js', '.ts', '.tsx']
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      fallback: {
+        ...config.resolve.fallback,
+        module: false,
+      }
+    };
+    return config;
+  },
+  // experimental: {
+  //   esmExternals: 'loose'
+  // }
 };
 
 module.exports = nextConfig;
